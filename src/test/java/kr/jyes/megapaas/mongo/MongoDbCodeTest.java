83ed1e0da8683cd8f78d265c0f168cb4c3cb415c
kikimans <kikimans@gmail.com>
2014-10-28 00:53:16 +0900

    repo add

--- src/test/java/kr/jyes/megapaas/mongo/MongoDbCodeTest.java
+++ src/test/java/kr/jyes/megapaas/mongo/MongoDbCodeTest.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 import kr.jyes.megapaas.model.CommonCode;
 import kr.jyes.megapaas.model.OpenShiftStatus;
+import kr.jyes.megapaas.repository.ICodeRepository;
 import kr.jyes.megapaas.service.IMongoService;
 
 import org.bson.types.ObjectId;
@@ -43,7 +44,7 @@ public class MongoDbCodeTest {
 	private MongoTemplate mongoTemplate;
 	
 	@Autowired
-	private IMongoService mongoService;
+	private ICodeRepository codeRepo;
 
 	@Before
 	public void setUp() throws Exception {
@@ -58,12 +59,12 @@ public class MongoDbCodeTest {
 	}
 	
 	@Test
-	public void test_mongoDB_Collection_가져오기(){
+	public void test_mongoDB_Collection(){
 		Set<String> mCollectionSet = mongoTemplate.getCollectionNames();
-		assertThat(mCollectionSet.isEmpty(), is(false));
-//		for(Iterator iter = mCollectionSet.iterator(); iter.hasNext();){
-//			logger.info("Collection Name : " + iter.next());
-//		}
+		//assertThat(mCollectionSet.isEmpty(), is(false));
+		for(Iterator iter = mCollectionSet.iterator(); iter.hasNext();){
+			logger.info("Collection Name : " + iter.next());
+		}
 	}
 	
 	@Test
@@ -73,7 +74,7 @@ public class MongoDbCodeTest {
 		code.setCode(1000);
 		code.setValue("1");
 		code.setName("status_cd");
-		code.setDesc("사용자승인상태");
+		code.setDesc("사용자 신청 상태");
 		code.setUseYn("Y");
 		code.setRegister("kikimans");	
 		
@@ -87,55 +88,87 @@ public class MongoDbCodeTest {
 	}
 	
 	@Test
-	public void test_mongoDB_Code_Collection_Refactor(){
+	public void test_mongoDB_Code_Collection_Create_Refactor(){
 		CommonCode code = new CommonCode();
 		
-		
+		code.setId(new ObjectId());
 		code.setCode(1000);
-		code.setValue("3");
+		code.setValue("4");
 		code.setName("status_cd");
-		code.setDesc("사용자승인대기상태");
-		code.setUseYn("N");
+		code.setDesc("사용자 대기 상태");
+		code.setUseYn("Y");
 		code.setRegister("kikimans");	
 		
-		mongoService.saveCode(code);	
+		codeRepo.createCode(code);	
 		
-		List<CommonCode> codelist = mongoService.getCodeAll();
+		List<CommonCode> codelist = mongoTemplate.find(new Query(Criteria.where("code").is(1000).and("value").is("3")), CommonCode.class);
 		
-		for(CommonCode cd : codelist){
-			System.out.println(cd);
-		}	
+		assertThat(codelist.size(), is(1));
+			
 	}
 	
 	@Test
-	public void test_mongoDB_Code_삭제(){
-		List<CommonCode> codelist = mongoTemplate.findAll(CommonCode.class);
+	public void test_code_update(){
+		List<CommonCode> codelist = mongoTemplate.find(new Query(Criteria.where("code").is(1000).and("value").is("3")), CommonCode.class);
 		
-		for(CommonCode cd : codelist){
-			mongoTemplate.remove(cd);
-		}	
+		CommonCode code = codelist.get(0);
+		
+		code.setRegister("tester");
+		
+		
+		
+		codeRepo.updateCode(code);
 	}
-	
-	@Test
-	public void test_mongoDB_Code_status_cd_가져오기(){
-		
-		Query query = new Query(Criteria.where("name").is("status_cd").and("useYn").is("Y"));
-		
-		List<CommonCode> status_cd_list = mongoTemplate.find(query, CommonCode.class);
-		
-		for(CommonCode cd : status_cd_list){
-			System.out.println(cd);
-		}	
-	}
-	
-	@Test
-	public void test_Code_데이터_전부_가져오기(){
-		List<CommonCode> codelist = mongoTemplate.findAll(CommonCode.class);
-		
-		for(CommonCode cd : codelist){
-			System.out.println(cd);
-		}
-	}
+//	
+//	@Test
+//	public void test_mongoDB_Code_Collection_Refactor2(){
+//		List<CommonCode> codelist = codeRepo.listCode();
+//	}
+//	
+//	@Test
+//	public void test_mongoDB_Code_삭제(){
+//		List<CommonCode> codelist = mongoTemplate.findAll(CommonCode.class);
+//		
+//		for(CommonCode cd : codelist){
+//			mongoTemplate.remove(cd);
+//		}	
+//	}
+//	
+//	@Test
+//	public void test_mongoDB_Code_삭제_refactor(){
+//		List<CommonCode> codelist = codeRepo.listCode();
+//		
+//		for(CommonCode cd : codelist){
+//			codeRepo.deleteCode(cd);
+//		}	
+//	}
+//	
+//	@Test
+//	public void test_mongoDB_Code_업데이트(){
+//		Query query = new Query(Criteria.where("name").is("stauts_cd").and("useYn").is("Y"));
+//		CommonCode code = codeRepo.findById(new Object());
+//	}
+//	
+//	@Test
+//	public void test_mongoDB_Code_status_cd_가져오기(){
+//		
+//		Query query = new Query(Criteria.where("name").is("status_cd").and("useYn").is("Y"));
+//		
+//		List<CommonCode> status_cd_list = mongoTemplate.find(query, CommonCode.class);
+//		
+//		for(CommonCode cd : status_cd_list){
+//			System.out.println(cd);
+//		}	
+//	}
+//	
+//	@Test
+//	public void test_Code_데이터_전부_가져오기(){
+//		List<CommonCode> codelist = mongoTemplate.findAll(CommonCode.class);
+//		
+//		for(CommonCode cd : codelist){
+//			System.out.println(cd);
+//		}
+//	}
 	
 //	@Test
 //	public void test_mongoDB_OpenShiftStatus_Collection(){
